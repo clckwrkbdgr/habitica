b221ad9775832145d4bbee3e6869a0ddd1efae67
Dixie Flatline <umi0451@gmail.com>
2016-10-29 09:40:47 +0300

    Considering local timezone for the start date of repeatable dailies.

--- habitica/core.py
+++ habitica/core.py
@@ -19,6 +19,7 @@ import netrc
 import os.path
 import datetime
 import sys
+import time
 from time import sleep
 from webbrowser import open_new_tab
 
@@ -176,12 +177,41 @@ def updated_task_list(tasks, tids):
         del(tasks[tid])
     return tasks
 
+class LocalTZ(datetime.tzinfo):
+    STDOFFSET = datetime.timedelta(seconds = -time.timezone)
+    DSTOFFSET = datetime.timedelta(seconds = -time.altzone) if time.daylight else STDOFFSET
+    DSTDIFF = DSTOFFSET - STDOFFSET
+    def utcoffset(self, dt):
+        if self._isdst(dt):
+            return LocalTZ.DSTOFFSET
+        else:
+            return LocalTZ.STDOFFSET
+    def dst(self, dt):
+        if self._isdst(dt):
+            return LocalTZ.DSTDIFF
+        else:
+            return datetime.timedelta(0)
+    def tzname(self, dt):
+        return time.tzname[self._isdst(dt)]
+    def _isdst(self, dt):
+        tt = (dt.year, dt.month, dt.day,
+              dt.hour, dt.minute, dt.second,
+              dt.weekday(), 0, 0)
+        stamp = time.mktime(tt)
+        tt = time.localtime(stamp)
+        return tt.tm_isdst > 0
+
+def strptime_habitica_to_local(time_string):
+    ''' Habitica's task start time is in GMT (apparently?)
+    so it needs to be converted to local TZ before calculating any task repetition.
+    '''
+    return datetime.datetime.strptime(time_string, '%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=datetime.timezone.utc).astimezone(LocalTZ())
 
 def print_task_list(tasks):
     for i, task in enumerate(tasks):
         if 'type' in task and task['type'] == 'daily':
             if task['frequency'] == 'daily':
-                startdate = datetime.datetime.strptime(task['startDate'], '%Y-%m-%dT%H:%M:%S.%fZ').date()
+                startdate = strptime_habitica_to_local(task['startDate']).date()
                 currentdate = datetime.datetime.today().date()
                 if (currentdate - startdate).days % task['everyX'] != 0:
                     continue
