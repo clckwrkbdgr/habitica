28f7894161bab27a5b21c703bb4e719bbcdcc444
Dixie Flatline <umi0451@gmail.com>
2020-11-09 16:58:42 +0200

    Basic API facade.

--- habitica/api.py
+++ habitica/api.py
@@ -1,29 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-"""
-Phil Adams http://philadams.net
-
-Python wrapper around the Habitica (http://habitica.com) API
-http://github.com/philadams/habitica
-"""
-
-
 import sys
 import json
 import logging
 logging.captureWarnings(True)
-
 import requests
 import requests.adapters
 import urllib3
 import urllib3.util.retry
 logging.getLogger('urllib3.connectionpool').setLevel(logging.CRITICAL)
 
-API_URI_BASE = 'api/v3'
-API_CONTENT_TYPE = 'application/json'
+class API(object):
+    """ Basic API facade. """
+    CONTENT_TYPE = 'application/json'
+    URI_BASE = 'api/v3'
+    TIMEOUT = 10.0
 
-TIMEOUT = 10.0
+    def __init__(self, auth):
+        """ Creates authenticated API instance. """
+        self.auth = auth
+        self.headers = auth if auth else {}
+        self.headers.update({'content-type': API.CONTENT_TYPE})
+    def get_url(self, *parts):
+        """ Makes URL to call specified .../subpath/of/parts. """
+        return '/'.join([self.auth['url'], API.URI_BASE] + list(parts))
+    def call(self, method, uri, data):
+        """ Performs actual call to URI using given method (POST/GET etc).
+        Data should correspond to specified method.
+        For POST/PUT methods, if field '_params' is present,
+        it is extracted and passed as request params.
+        """
+        session = requests.Session()
+        retries = urllib3.util.retry.Retry(total=5, backoff_factor=0.1)
+        session.mount('https://', requests.adapters.HTTPAdapter(max_retries=retries))
+
+        if method in ['put', 'post']:
+            params = data.get('_params', None)
+            if '_params' in data:
+                del data['_params']
+            return getattr(session, method)(uri, headers=self.headers,
+                    params=params, data=json.dumps(data), timeout=API.TIMEOUT)
+        else:
+            return getattr(session, method)(uri, headers=self.headers,
+                                            params=data, timeout=API.TIMEOUT)
+
 MAX_RETRY = 3
 
 def dump_errors(errors):
@@ -36,21 +56,19 @@ class Habitica(object):
     A minimalist Habitica API class.
     """
 
-    def __init__(self, auth=None, resource=None, aspect=None):
-        self.auth = auth
+    def __init__(self, auth=None, resource=None, aspect=None, _api=None):
+        self.api = _api or API(auth)
         self.resource = resource
         self.aspect = aspect
-        self.headers = auth if auth else {}
-        self.headers.update({'content-type': API_CONTENT_TYPE})
 
     def __getattr__(self, m):
         try:
             return object.__getattr__(self, m)
         except AttributeError:
             if not self.resource:
-                return Habitica(auth=self.auth, resource=m)
+                return Habitica(_api=self.api, resource=m)
             else:
-                return Habitica(auth=self.auth, resource=self.resource,
+                return Habitica(_api=self.api, resource=self.resource,
                                 aspect=m)
 
     def __getitem__(self, m):
@@ -58,10 +76,10 @@ class Habitica(object):
             return object.__getitem__(self, m)
         except AttributeError:
             if not self.resource:
-                return Habitica(auth=self.auth, resource=m)
+                return Habitica(_api=self.api, resource=m)
             else:
                 res = self.resource + '/' + str(m)
-                return Habitica(auth=self.auth, resource=res)
+                return Habitica(_api=self.api, resource=res)
 
     def __call__(self, **kwargs):
         method = kwargs.pop('_method', 'get')
@@ -73,22 +91,18 @@ class Habitica(object):
             aspect_id = kwargs.pop('_id', None)
             direction = kwargs.pop('_direction', None)
             if aspect_id is not None:
-                uri = '%s/%s/%s/%s/%s' % (self.auth['url'],
-                                          API_URI_BASE,
+                uri = self.api.get_url(
                                           self.resource,
                                           self.aspect,
                                           str(aspect_id))
             else:
-                uri = '%s/%s/%s/%s' % (self.auth['url'],
-                                       API_URI_BASE,
+                uri = self.api.get_url(
                                        self.resource,
                                        self.aspect)
             if direction is not None:
                 uri = '%s/%s' % (uri, direction)
         else:
-            uri = '%s/%s/%s' % (self.auth['url'],
-                                API_URI_BASE,
-                                self.resource)
+            uri = self.api.get_url(self.resource)
         return self.try_call(method, uri, kwargs)
 
     def try_call(self, method, uri, kwargs, tries=MAX_RETRY, messages=None):
@@ -110,18 +124,7 @@ class Habitica(object):
             return dump_errors(messages + [e])
 
     def actual_call(self, method, uri, kwargs):
-        session = requests.Session()
-        retries = urllib3.util.retry.Retry(total=5, backoff_factor=0.1)
-        session.mount('https://', requests.adapters.HTTPAdapter(max_retries=retries))
-        if method in ['put', 'post']:
-            params = kwargs.get('_params', None)
-            if '_params' in kwargs:
-                del kwargs['_params']
-            res = getattr(session, method)(uri, headers=self.headers,
-                    params=params, data=json.dumps(kwargs), timeout=TIMEOUT)
-        else:
-            res = getattr(session, method)(uri, headers=self.headers,
-                                            params=kwargs, timeout=TIMEOUT)
+        res = self.api.call(method, uri, kwargs)
 
         # print(res.url)  # debug...
         if res.status_code == requests.codes.ok:
