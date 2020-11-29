462f5f2f4ee9315378d5d6bf63245e08760b2216
Dixie Flatline <umi0451@gmail.com>
2020-11-29 17:25:47 +0200

    Challenges.

--- habitica/core.py
+++ habitica/core.py
@@ -26,6 +26,75 @@ class Content:
 		except AttributeError:
 			return self._data[key]
 
+class Challenge:
+	# TODO get challenge by id: get:/challenges/:id
+	def __init__(self, _data=None, _api=None):
+		self.api = _api
+		self._data = _data
+	@property
+	def id(self):
+		return self._data['id']
+	@property
+	def name(self):
+		return self._data['name']
+	@property
+	def shortName(self):
+		return self._data['shortName']
+	@property
+	def updatedAt(self):
+		return self._data['updatedAt'] # FIXME convert to date time using user TZ.
+	@property
+	def createdAt(self):
+		return self._data['createdAt'] # FIXME convert to date time using user TZ.
+	@property
+	def prize(self):
+		return self._data['prize']
+	@property
+	def memberCount(self):
+		return self._data['memberCount']
+	@property
+	def official(self):
+		return self._data['official']
+	def rewards(self):
+		return [Reward(_data=self.api.get('tasks', task_id).data, _api=self.api) for task_id in self._data['tasksOrder']['rewards']]
+	def todos(self):
+		return [Todo(_data=self.api.get('tasks', task_id).data, _api=self.api) for task_id in self._data['tasksOrder']['todos']]
+	def dailies(self):
+		return [Daily(_data=self.api.get('tasks', task_id).data, _api=self.api) for task_id in self._data['tasksOrder']['dailys']]
+	def habits(self):
+		return [Habit(_data=self.api.get('tasks', task_id).data, _api=self.api) for task_id in self._data['tasksOrder']['habits']]
+	def leader(self):
+		# FIXME get person profile by id.
+		return self._data['leader']
+	def group(self):
+		# FIXME get group by id.
+		return Group(_data=self._data['group'], _api=self.api)
+	def as_csv(self):
+		return self.api.get('challenges', self.id, 'export', 'csv')
+	def clone(self):
+		return Challenge(_api=self.api,
+				_data=self.api.post('challenges', self.id, 'clone').challenge,
+				)
+	def update(self, name=None, summary=None, description=None):
+		params = dict()
+		if name:
+			params['name'] = name
+		if description:
+			params['description'] = description
+		if summary:
+			params['summary'] = summary
+		if not params:
+			return
+		self._data = self.api.put('challenges', self.id, _body=params).data
+	def join(self):
+		self._data = self.api.post('challenges', self.id, 'join').data
+	def leave(self):
+		self.api.post('challenges', self.id, 'leave')
+	def selectWinner(self, person):
+		self.api.post('challenges', self.id, 'selectWinner', person.id)
+	def delete(self):
+		self.api.delete('challenges', self.id)
+
 class ChatMessage:
 	def __init__(self, _data=None):
 		self._data = _data
@@ -52,6 +121,8 @@ class Group:
 	PUBLIC_GUILDS = 'publicGuilds'
 	TAVERN = 'tavern'
 
+	PRIVATE, PUBLIC = 'private', 'public'
+
 	def __init__(self, _data=None, _api=None):
 		self.api = _api
 		self._data = _data
@@ -61,10 +132,39 @@ class Group:
 	@property
 	def name(self):
 		return self._data['name']
+	@property
+	def type(self):
+		return self._data['type']
+	@property
+	def privacy(self):
+		return self._data['privacy']
+	def challenges(self):
+		return [Challenge(_data=entry, _api=self.api) for entry in self.api.get('challenges', 'groups', self.id).data]
 	def chat(self):
 		return [ChatMessage(entry) for entry in self.api.get('groups', self.id, 'chat').data]
 	def mark_chat_as_read(self):
 		self.api.post('groups', self.id, 'chat', 'seen')
+	def create_challenge(self, name, shortName, summary=None, description=None, prize=0):
+		""" Creates challenge with specified name and tag (shortName).
+		Summary and description are optional.
+		Prize is a number of gems (optional, default is 0).
+		"""
+		params = {
+				'group' : self.id,
+				'name' : name,
+				'shortName' : shortName,
+				}
+		if summary:
+			params['summary'] = summary[:250]
+		if description:
+			params['description'] = description
+		if prize:
+			params['prize'] = int(prize)
+		# TODO body['official'] (for admins only, need roles).
+		data = self.api.post('challenges', _body={
+			'challenge' : params,
+			}).data
+		return Challenge(_api=self.api, _data=data)
 
 class Quest:
 	def __init__(self, _data=None, _api=None):
@@ -438,6 +538,8 @@ class User:
 		return self._proxy.dailies()
 	def rewards(self):
 		return self._proxy.rewards()
+	def challenges(self):
+		return self._proxy.challenges()
 	def spells(self):
 		""" Returns list of available spells. """
 		SPELLS = { # TODO apparently /content lists these.
@@ -494,6 +596,8 @@ class _UserProxy:
 		return [Todo(_data=entry, _api=self.api) for entry in self.api.get('tasks', 'user', type='todos').data]
 	def rewards(self):
 		return [Reward(_data=entry, _api=self.api) for entry in self.api.get('tasks', 'user', type='rewards').data]
+	def challenges(self):
+		return [Challenge(_data=entry, _api=self.api) for entry in self.api.get('challenges', 'user').data]
 
 class Habitica:
 	""" Main Habitica entry point. """
